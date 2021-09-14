1aaf6e817724a6a693c1b958c041cc60438f66bb
Dialvec <diavelandiaca@unal.edu.co>
2021-09-14 08:18:05 -0500

    Agregamos comentario en archivo 1.wikipedia

--- NIVEL 1/1_wikipedia.py
+++ NIVEL 1/1_wikipedia.py
@@ -9,6 +9,8 @@ ULTIMA VEZ EDITADO: 12 ABRIL 2020
 import requests # pip install requests
 from lxml import html # pip install lxml
 
+#Comentario Agregado
+
 # USER AGENT PARA PROTEGERNOS DE BANEOS
 headers = {
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36",
